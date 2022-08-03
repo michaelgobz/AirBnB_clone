39447313b271b8bd2522e3ca82b8419fc8d495e8
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-08-03 08:54:17 +0000

    reload all objects on basemodel import

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,10 @@
-__all__ = []
+#!/usr/bin/python3
+"""
+Init file for models package
+Create variable storage an instance of fileStorage
+"""
+
+from models.engine import file_storage
+
+storage = file_storage.FileStorage()
+storage.reload()
