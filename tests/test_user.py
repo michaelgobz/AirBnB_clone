776d2e401f044f6e419c63a9c8b58aa1ffee6e4d
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-08-03 09:28:05 +0000

    review test case

---
+++ tests/test_user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module test_user
+Contains unitest for User class
+Which inherits from BaseModel
+"""
+
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+
+
+class UserTestCase(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
